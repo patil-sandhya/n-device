9b20094906c5c218998263d7331cf461b1f7ff22
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-11-14 22:28:48 +0530

    added health check route

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,10 @@ app.use("/user", userRoutes);
 const PORT = process.env.PORT || 5000;
 connectDB();
 
+app.get('/', (req, res) => {
+  res.send('Server is up and running 🚀');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
