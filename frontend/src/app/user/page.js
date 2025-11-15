4c56e21ba4df8c4ddb2d97cb6980cf6b13ec4554
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-11-15 08:31:51 +0530

    added suspence tag

--- frontend/src/app/user/page.js
+++ frontend/src/app/user/page.js
@@ -1,12 +1,14 @@
 import UserProfile from '@/components/UserProfile'
 import React from 'react'
+import { Suspense } from 'react';
 
 const page = () => {
   return (
     <div>
+      <Suspense>
         <UserProfile />
+        </Suspense>
     </div>
   )
 }
-
 export default page
